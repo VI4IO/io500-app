VI4IO/io500-app

commit 725acb3637614b67ca4ae59467691b464c75602e

main: add IO500_MINWRITE constant
Add a constant for the 300s minimum runtime, rather than
hard-coding it in several places in the code.  It will
eventually also needed for Student Cluster Competition.

Use it to print "-invalid" markers for any subtests that
didn't run for a long enough time.  That should never
happen, but it makes the bad result(s) easier to see.

Signed-off-by: Andreas Dilger <adilger@dilger.ca>

src/phase_dbg.c
@@ -1,13 +1,15 @@
 #include <io500-phase.h>
 
+#define STRINGIFY(foo)  #foo
+
 static ini_option_t option[] = {
-  {"stonewall-time", "The stonewall timer, set to a smaller value for testing", 0, INI_UINT, "300", & opt.stonewall},
+  {"stonewall-time", "The stonewall timer, set to a smaller value for testing", 0, INI_UINT, STRINGIFY(IO500_MINWRITE), & opt.stonewall},
   {NULL} };
 
 
 static void validate(void){
-  if(opt.stonewall != 300 && opt.rank == 0){
-    INVALID("stonewall-time != 300\n");
+  if(opt.stonewall != IO500_MINWRITE && opt.rank == 0){
+    INVALID("stonewall-time != %us\n", IO500_MINWRITE);
   }
 }
 

include/io500-phase.h
@@ -32,6 +32,7 @@ typedef struct{
   io500_phase_score_group group;
 } u_phase_t;
 
+#define IO500_MINWRITE 300
 #define IO500_PHASES (2 + 1 + 2*3 + 1 + 4 + 5)
 
 extern u_phase_t p_opt;

src/main.c
@@ -269,7 +269,10 @@ int main(int argc, char ** argv){
 
         char score_str[40];
         sprintf(score_str, "%f", score);
-        dupprintf("[RESULT] %20s %15s %s : time %.3f seconds\n", phase->name, score_str, phase->name[0] == 'i' ? "GiB/s " : "kIOPS", runtime);
+        dupprintf("[RESULT%s] %20s %15s %s : time %.3f seconds\n",
+		  phase->type == IO500_PHASE_WRITE && runtime < IO500_MINWRITE ?
+			"-invalid" : "",
+		  phase->name, score_str, phase->name[0] == 'i' ? "GiB/s " : "kIOPS", runtime);
       }
       u_hash_update_key_val_dbl(& score_hash, phase->name, score);
     }
@@ -330,7 +333,7 @@ int main(int argc, char ** argv){
     PRINT_PAIR("hash", "%X\n", (int) score_hash);
 
     dupprintf("[SCORE%s] Bandwidth %f GB/s : IOPS %f kiops : TOTAL %f\n",
-      opt.is_valid_run ? "" : " INVALID",
+      opt.is_valid_run ? "" : "-invalid",
       scores[IO500_SCORE_BW], scores[IO500_SCORE_MD], overall_score);
   }
 

commit 10277b165d31b34ddb6152d714f6d5920e1ee5fe

io500.sh: simplify configuration for users
Move the setup_paths() function to the top of the io500.sh script,
since this should be the only thing that users need to change here.

Add a config-full.ini example file that contains all of the
parameters from "io500 --list" so users don't need to know that.

Fix stringification of IO500_MINWRITE in "io500 --list" output.

Fixes: 725acb363761 ("main: add IO500_MINWRITE constant")
Signed-off-by: Andreas Dilger <adilger@dilger.ca>

config-full.ini
@@ -3,10 +3,8 @@
 datadir = ./datafiles
 # The data directory is suffixed by a timestamp. Useful for running several IO500 tests concurrently.
 timestamp-datadir = TRUE
-
 # The result directory.
 resultdir = ./results
-
 # The result directory is suffixed by a timestamp. Useful for running several IO500 tests concurrently.
 timestamp-resultdir = TRUE
 # The API to create/delete the datadir
@@ -19,8 +17,8 @@ drop-caches-cmd = sudo -n bash -c "echo 3 > /proc/sys/vm/drop_caches"
 verbosity = 1
 
 [debug]
-# The stonewall timer, must be at least 300s for a valid test run
-stonewall-time = 300 # 300s for full run
+# Stonewall timer must be 300 for a valid result, can be smaller for testing
+stonewall-time = 300
 
 [ior-easy]
 # The API to be used
@@ -30,13 +28,25 @@ posix.odirect =
 # Transfer size
 transferSize = 2m
 # Block size; must be a multiple of transferSize
-blockSize = 2m # 1024000m
+blockSize = 9920000m
 # Filename for MPI hint file
 hintsFileName = 
 # Create one file per process
 filePerProc = TRUE
 # Use unique directory per file per process
 uniqueDir = FALSE
+# Disable running of this phase
+noRun = 
+# The verbosity level
+verbosity = 
+
+[ior-easy-write]
+# The API to be used
+API = 
+# Use ODirect
+posix.odirect = 
+# Filename for hints file
+hintsFileName = 
 
 [mdtest-easy]
 # The API to be used
@@ -45,14 +55,64 @@ API = POSIX
 posix.odirect = 
 # Files per proc
 n = 1000000
+# Disable running of this phase
+noRun = 
+
+[mdtest-easy-write]
+# The API to be used
+API = 
+# Use ODirect
+posix.odirect = 
+# Disable running of this phase
+noRun =
 
 [ior-hard]
+# The API to be used
+API = POSIX
+# Use ODirect
+posix.odirect = 
 # Filename for hints file
 hintsFileName = 
 # Number of segments
 segmentCount = 10000000
+# Disable running of this phase
+noRun =
+# The verbosity level
+verbosity = 
+
+[ior-hard-write]
+# The API to be used
+API = 
+# Use ODirect
+posix.odirect = 
+# Filename for hints file
+hintsFileName = 
+
+[mdtest-hard]
+# The API to be used
+API = POSIX
+# Use ODirect
+posix.odirect = 
+# Files per proc
+n = 1000000
+# Disable running of this phase
+noRun =
+
+[mdtest-hard-write]
+# The API to be used
+API = 
+# Use ODirect
+posix.odirect = 
+# Disable running of this phase
+noRun =
 
 [find]
+# Set to an external script to perform the find phase
+external-script = 
+# Extra arguments for external scripts
+external-extra-args = 
+# Startup arguments for external scripts
+external-mpi-args = 
 # Set the number of processes for pfind
 nproc = 
 # Pfind queue length
@@ -60,4 +120,4 @@ pfind-queue-length = 10000
 # Pfind Steal from next
 pfind-steal-next = FALSE
 # Parallelize the readdir by using hashing. Your system must support this!
-pfind-parallelize-single-dir-access-using-hashing = TRUE
+pfind-parallelize-single-dir-access-using-hashing = FALSE

src/phase_dbg.c
@@ -1,9 +1,10 @@
 #include <io500-phase.h>
 
-#define STRINGIFY(foo)  #foo
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
 
 static ini_option_t option[] = {
-  {"stonewall-time", "The stonewall timer, set to a smaller value for testing", 0, INI_UINT, STRINGIFY(IO500_MINWRITE), & opt.stonewall},
+  {"stonewall-time", "Stonewall timer must be "TOSTRING(IO500_MINWRITE)" for a valid result, can be smaller for testing", 0, INI_UINT, TOSTRING(IO500_MINWRITE), & opt.stonewall},
   {NULL} };
 
 